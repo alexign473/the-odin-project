f78f23cff92613c0077aa2cd66db4a719612c684
Alexander Ignatev <alexign473@gmail.com>
2022-11-11 06:54:40 +0300

    Add UI, refactor game logic

--- 02-rock-paper-scissors/script.js
+++ 02-rock-paper-scissors/script.js
@@ -1,47 +1,22 @@
+let playerScore = 0;
+let computerScore = 0;
+
+const results = document.querySelector('.results');
+const score = document.querySelector('.score');
+const buttons = document.querySelectorAll('button');
+
 function getComputerChoice() {
-  const choice = Math.floor(Math.random() * 3);
-  switch (choice) {
-    case 0:
-      return 'rock';
-      break;
-    case 1:
-      return 'paper';
-      break;
-    case 2:
-      return 'scissors';
-      break;
-  }
+  const choices = ['rock', 'paper', 'scissors'];
+  return choices[Math.floor(Math.random() * choices.length)];
 }
-
-function getUserInput() {
-  let userInput = prompt('Enter rock, paper or scissors');
-  if (!userInput) return;
-  userInput = userInput.toLowerCase();
-  if (
-    userInput === 'rock' ||
-    userInput === 'paper' ||
-    userInput === 'scissors'
-  ) {
-    return userInput;
-  } else {
-    return getUserInput();
-  }
-}
-
-/*
-Write a function that plays a single round of Rock Paper Scissors. 
-The function should take two parameters - the playerSelection and computerSelection - 
-and then return the winner of the round and log like so: "You Lose! Paper beats Rock"
-*/
 
 function playRound(playerSelection, computerSelection) {
-  // Make function’s playerSelection parameter case-insensitive
-  playerSelection = playerSelection.toLowerCase();
+  let result = '';
   // Compare inputted parameters
-  // If player and computer choices are equal return string "tie"
+  // If player and computer choices are equal it's a tie
   if (playerSelection === computerSelection) {
-    console.log('Tie');
-    return 'tie';
+    result = 'Tie';
+    return result;
   }
   // Computer wincon
   if (
@@ -49,8 +24,9 @@ function playRound(playerSelection, computerSelection) {
     (playerSelection === 'paper' && computerSelection === 'scissors') ||
     (playerSelection === 'scissors' && computerSelection === 'rock')
   ) {
-    console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
-    return 'computer';
+    result = `You Lose! ${computerSelection} beats ${playerSelection}`;
+    computerScore++;
+    return result;
   }
   // Player wincon
   if (
@@ -58,10 +34,39 @@ function playRound(playerSelection, computerSelection) {
     (playerSelection === 'paper' && computerSelection === 'rock') ||
     (playerSelection === 'scissors' && computerSelection === 'paper')
   ) {
-    console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
-    return 'player';
+    result = `You Win! ${playerSelection} beats ${computerSelection}`;
+    playerScore++;
+    return result;
+  }
+  return result;
+}
+
+function isGameOver() {
+  return playerScore >= 5 || computerScore >= 5;
+}
+
+function game(e) {
+  if (isGameOver()) return;
+  const playerSelection = e.target.id;
+  const computerSelection = getComputerChoice();
+  // Call playRound function with user input
+  const roundResult = playRound(playerSelection, computerSelection);
+  results.textContent = roundResult;
+  score.textContent = `Player score: ${playerScore}, Computer score: ${computerScore}`;
+
+  if (playerScore >= 5) {
+    console.log('player win');
+    results.textContent = 'End of the game! PLAYER WIN';
+    return;
+  }
+  if (computerScore >= 5) {
+    console.log('computer win');
+    results.textContent = 'End of the game! COMP WIN';
+    return;
   }
 }
+
+buttons.forEach((button) => button.addEventListener('click', game));
 
 /*
 Write a NEW function called game(). 
@@ -69,38 +74,38 @@ Call the playRound function inside of this one to play a 5 round game
 that keeps score and reports a winner or loser at the end.
 */
 
-function game() {
-  // Create variable of player score
-  let playerScore = 0;
-  // Create variable of computer score
-  let computerScore = 0;
+// function game() {
+//   // Create variable of player score
+//   let playerScore = 0;
+//   // Create variable of computer score
+//   let computerScore = 0;
 
-  // Loop from 0 to 5
-  for (let i = 0; i < 5; i++) {
-    // Get user input
-    const playerSelection = getUserInput();
-    if (!playerSelection) {
-      return;
-    }
-    const computerSelection = getComputerChoice();
-    // Call playRound function with user input
-    const roundWinner = playRound(playerSelection, computerSelection);
-    // If player wins, add 1 to player score
-    // If computer wins, add 1 to computer score
-    if (roundWinner === 'player') {
-      playerScore++;
-    } else if (roundWinner === 'computer') {
-      computerScore++;
-    }
-  }
-  // Report a winner
-  if (playerScore === computerScore) {
-    return "It's a Tie";
-  } else if (playerScore > computerScore) {
-    return 'Player won!';
-  } else {
-    return 'Computer won!';
-  }
-}
+//   // Loop from 0 to 5
+//   for (let i = 0; i < 5; i++) {
+//     // Get user input
+//     const playerSelection = getUserInput();
+//     if (!playerSelection) {
+//       return;
+//     }
+//     const computerSelection = getComputerChoice();
+//     // Call playRound function with user input
+//     const roundWinner = playRound(playerSelection, computerSelection);
+//     // If player wins, add 1 to player score
+//     // If computer wins, add 1 to computer score
+//     if (roundWinner === 'player') {
+//       playerScore++;
+//     } else if (roundWinner === 'computer') {
+//       computerScore++;
+//     }
+//   }
+//   // Report a winner
+//   if (playerScore === computerScore) {
+//     return "It's a Tie";
+//   } else if (playerScore > computerScore) {
+//     return 'Player won!';
+//   } else {
+//     return 'Computer won!';
+//   }
+// }
 
-console.log('End of the game!', game());
+// console.log('End of the game!', game());
