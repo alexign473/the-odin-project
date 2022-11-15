fcadbacf6aa3f89b08cad924e3469a6cd316b50a
Alexander Ignatev <alexign473@gmail.com>
2022-11-15 10:15:06 +0300

    Add size and reset buttons

--- 03-etch-a-sketch/main.js
+++ 03-etch-a-sketch/main.js
@@ -1,11 +1,15 @@
-const container = document.querySelector('.container');
+let size = 100;
 
-function createGrid(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (let c = 0; c < rows * cols; c++) {
+const container = document.querySelector('.container');
+const sizeBtn = document.querySelector('#size');
+const resetBtn = document.querySelector('#reset');
+
+function createGrid(size) {
+  container.style.setProperty('--grid-rows', size);
+  container.style.setProperty('--grid-cols', size);
+  for (let c = 0; c < size * size; c++) {
     const cell = document.createElement('div');
-    cell.classList.add('cell');
+    // cell.classList.add('cell');
     cell.addEventListener('mouseover', dye);
     container.append(cell);
   }
@@ -15,4 +19,29 @@ function dye(e) {
   e.target.style.backgroundColor = 'black';
 }
 
-createGrid(100, 100);
+function resetCanvas() {
+  container.innerHTML = '';
+  createGrid(size);
+}
+
+function changeSize() {
+  const input = getUserInput();
+  if (!input) return;
+  size = input;
+  resetCanvas();
+}
+
+function getUserInput() {
+  const input = +prompt(
+    'On a scale of 16-100, how big would you like your canvas?',
+    16
+  );
+  if (!input) return;
+  if (input > 100 || input < 16) return getUserInput();
+  return input;
+}
+
+createGrid(size);
+
+sizeBtn.addEventListener('click', changeSize);
+resetBtn.addEventListener('click', resetCanvas);
