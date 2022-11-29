dd77225d17cb1e00c0a5f40b8d6e06d8ce26abfe
Alexander Ignatev <alexign473@gmail.com>
2022-11-29 17:21:16 +0300

    Add operate function

--- 04-calculator/main.js
+++ 04-calculator/main.js
@@ -10,3 +10,12 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+function operate(a, b, operator) {
+  if (operator) return operator(a, b);
+}
+
+console.log(operate(3, 2, add));
+console.log(operate(3, 2, subtract));
+console.log(operate(3, 2, multiply));
+console.log(operate(3, 2, divide));
