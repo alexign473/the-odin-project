48b6edb137079db479b6afc2890ab2e742c23d73
Alexander Ignatev <alexign473@gmail.com>
2022-11-30 16:49:07 +0300

    Add working calculations

--- 04-calculator/main.js
+++ 04-calculator/main.js
@@ -1,15 +1,9 @@
-function add(a, b) {
-  return a + b;
-}
-function subtract(a, b) {
-  return a - b;
-}
-function multiply(a, b) {
-  return a * b;
-}
-function divide(a, b) {
-  return a / b;
-}
+const actions = {
+  add: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => a / b,
+};
 
 function operate(a, b, operator) {
   if (operator) return operator(a, b);
@@ -18,15 +12,34 @@ function operate(a, b, operator) {
 const expression = document.querySelector('.expression');
 const result = document.querySelector('.result');
 const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator');
+const equal = document.querySelector('.equal');
 
-// on a number button click
-// take number value and store it in a variable
-// add this variable as text content to the div
-let displayValue = '';
+let inputValue = '';
+let firstNum = null;
+let operator = null;
 
 function fillDisplay(e) {
-  displayValue += e.target.value;
-  result.textContent = displayValue;
+  inputValue += e.target.value;
+  result.textContent = inputValue;
+}
+
+function handleOperator(e) {
+  operator = e.target.value;
+  result.textContent += e.target.textContent;
+
+  if (!firstNum) {
+    firstNum = +inputValue;
+  }
+  inputValue = '';
 }
 
 numbers.forEach((num) => num.addEventListener('click', fillDisplay));
+
+operators.forEach((op) => op.addEventListener('click', handleOperator));
+
+equal.addEventListener('click', function () {
+  const res = operate(firstNum, +inputValue, actions[operator]);
+  result.textContent = res;
+  firstNum = res;
+});
